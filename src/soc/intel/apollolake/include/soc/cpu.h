83713b63e0f64f412479584714186d65d09a884a
Alexandru Gagniuc <alexandrux.gagniuc@intel.com>
2015-10-06 16:31:34 -0700

    soc/apollolake: Add tsc_freq_mhz() functionality for udelay()
    Change-Id: I2f1147cefe2438992bff45fc0a1e91064217915d
    Signed-off-by: Alexandru Gagniuc <alexandrux.gagniuc@intel.com>

---
+++ src/soc/intel/apollolake/include/soc/cpu.h
@@ -0,0 +1,20 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2015 Intel Corp.
+ * (Written by Alexandru Gagniuc <alexandrux.gagniuc@intel.com> for Intel Corp.)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef _SOC_APOLLOLAKE_CPU_H_
+#define _SOC_APOLLOLAKE_CPU_H_
+
+#define MSR_PLATFORM_INFO	0xce
+
+#define BASE_CLOCK_MHZ		100
+
+#endif /* _SOC_APOLLOLAKE_CPU_H_ */
