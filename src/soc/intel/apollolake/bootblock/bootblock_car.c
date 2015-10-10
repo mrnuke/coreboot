63aebf31f81200ef47e26171a4868e46f6c0f562
Alexandru Gagniuc <alexandrux.gagniuc@intel.com>
2015-10-09 17:34:46 -0700

    soc/apollolake: Disable watchdog timer after console initialization
    Change-Id: I0680d3866cece70e3ea03b5c1b22769149fc2278
    Signed-off-by: Alexandru Gagniuc <alexandrux.gagniuc@intel.com>

--- src/soc/intel/apollolake/bootblock/bootblock_car.c
+++ src/soc/intel/apollolake/bootblock/bootblock_car.c
@@ -12,8 +12,25 @@
 
 #include <arch/io.h>
 #include <console/console.h>
+#include <device/pci.h>
 #include <soc/bootblock.h>
 #include <soc/uart.h>
+
+static void disable_watchdog(void)
+{
+	uint32_t reg;
+	device_t dev = PCI_DEV(0, 0xd, 1);
+
+	/* Open up an IO window */
+	pci_write_config16(dev, PCI_BASE_ADDRESS_4, 0x400);
+	pci_write_config32(dev, PCI_COMMAND,
+			   PCI_COMMAND_MASTER | PCI_COMMAND_IO);
+
+	/* We don't have documentation for this bit, but it prevents reboots */
+	reg = inl(0x400 + 0x68);
+	reg |= 1 << 11;
+	outl(reg, 0x400 + 0x68);
+}
 
 void bootblock_car_main(void)
 {
@@ -24,6 +41,9 @@ void bootblock_car_main(void)
 		lpss_console_uart_init();
 		console_init();
 	}
+
+	/* Wait until after we have console to disable this */
+	disable_watchdog();
 
 	/* Don't return, so we see the above post code */
 	while (1)
