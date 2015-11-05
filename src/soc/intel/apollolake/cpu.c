44b37370e69e7236dc0c4032d6c647ae1e335ae4
Alexandru Gagniuc <alexandrux.gagniuc@intel.com>
2015-11-05 11:14:51 -0800

    soc/apollolake: Rename cpu.c to tsc_freq.c, add copyright header
    To be consistent with other platforms, cpu.c should contain the
    ramstage CPU drivers. Since other Intel SOCs use tsc_freq.c for the
    same purpose we used cpu.c, rename it accordingly.
    
    Change-Id: I4ce6d9d9b37cc28d3cd74f7dd795718a07c6e8d4
    Signed-off-by: Alexandru Gagniuc <alexandrux.gagniuc@intel.com>

--- src/soc/intel/apollolake/cpu.c
+++
@@ -1,9 +0,0 @@
-#include <cpu/x86/msr.h>
-#include <cpu/x86/tsc.h>
-#include <soc/cpu.h>
-
-unsigned long tsc_freq_mhz(void)
-{
-	msr_t msr = rdmsr(MSR_PLATFORM_INFO);
-	return (BASE_CLOCK_MHZ * ((msr.lo >> 8) & 0xff));
-}
