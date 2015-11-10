119bb78acd6e8cdf4200a551006a4d948a3e0d88
Alexandru Gagniuc <alexandrux.gagniuc@intel.com>
2015-11-09 17:07:18 -0800

    drivers/intel/fsp2_0: Add infrastructure to parse resource HOBs
    This adds the minimal infrastructure to allow printing reource HOBs
    returned by FSP memory_init, and to allow locating the FSP reserved
    memory area.
    
    Change-Id: Ie79441aacdb808200921817f537bf01e0456b72c
    Signed-off-by: Alexandru Gagniuc <alexandrux.gagniuc@intel.com>

--- src/drivers/intel/fsp2_0/include/fsp/util.h
+++ src/drivers/intel/fsp2_0/include/fsp/util.h
@@ -14,6 +14,20 @@
 #define _FSP2_0_UTIL_H_
 
 #include <fsp/info_header.h>
+#include <device/resource.h>
+
+/*
+ * Hand-off-block handling functions that depend on CBMEM, and thus can only
+ * be used after cbmem_initialize().
+ */
+void fsp_save_hob_list(void *hob_list_ptr);
+const void *fsp_get_hob_list(void);
+/*
+ * Hand-off-block utilities which do not depend on CBMEM, but need to be passed
+ * the HOB list explicitly.
+ */
+void fsp_find_reserved_memory(struct resource *res, const void *hob_list);
+void fsp_print_memory_resource_hobs(const void *hob_list);
 
 /* Load an FSP binary into CBFS, and fill the associated fsp_header struct */
 enum cb_err fsp_load_binary(struct fsp_header *hdr, const char *name);
