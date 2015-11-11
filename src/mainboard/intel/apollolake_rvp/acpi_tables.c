f66ad4a497fabe78cc5a4108976c411bd161549f
Lance Zhao <lijian.zhao@intel.com>
2015-11-11 15:14:29 -0800

    src/mainboard/intel/apollolake_rvp: Include FADT tables
    Include SOC specific FADT tables to current mainboard.
    
    Change-Id: Id4099528657304e9f7675c839e7666c58f189004
    Signed-off-by: Lance Zhao <lijian.zhao@intel.com>

--- src/mainboard/intel/apollolake_rvp/acpi_tables.c
+++ src/mainboard/intel/apollolake_rvp/acpi_tables.c
@@ -10,9 +10,33 @@
  * (at your option) any later version.
  */
 
-#include <arch/acpi.h>
+#include <string.h>
+#include <soc/acpi.h>
 
 void acpi_create_fadt(acpi_fadt_t *fadt, acpi_facs_t *facs, void *dsdt)
 {
-}
+	acpi_header_t *header = &(fadt->header);
 
+	memset(fadt, 0, sizeof(acpi_fadt_t));
+	memcpy(header->signature, "FACP", 4);
+	header->length = sizeof(acpi_fadt_t);
+	header->revision = 5;
+	memcpy(header->oem_id, OEM_ID, 6);
+	memcpy(header->oem_table_id, ACPI_TABLE_CREATOR, 8);
+	memcpy(header->asl_compiler_id, ASLC, 4);
+	header->asl_compiler_revision = 1;
+
+	fadt->firmware_ctrl = (uintptr_t)facs;
+	fadt->dsdt = (uintptr_t)dsdt;
+	fadt->model = 1;
+	fadt->preferred_pm_profile = PM_MOBILE;
+
+	fadt->x_firmware_ctl_l = (uintptr_t)facs;
+	fadt->x_firmware_ctl_h = 0;
+	fadt->x_dsdt_l = (uintptr_t)dsdt;
+	fadt->x_dsdt_h = 0;
+
+	soc_fill_common_fadt(fadt);
+
+	header->checksum = acpi_checksum((void *)fadt, header->length);
+}
