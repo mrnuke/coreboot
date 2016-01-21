cab3caafe67a64750b701fc4ec550ce6fa1112b4
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2016-01-21 13:35:59 -0800

    cpu/qemu-x86: Jump to generic bootblock after we do housekeeping
    Change-Id: I0623cb065bf4297da14c7d744aebc641f37e03b0
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- src/cpu/qemu-x86/bootblock.c
+++ src/cpu/qemu-x86/bootblock.c
@@ -10,6 +10,7 @@
  */
 
 #include <arch/cpu.h>
+#include <bootblock_common.h>
 #include <cbfs.h>
 #include <console/console.h>
 #include <cpu/x86/bist.h>
@@ -20,17 +21,12 @@ asmlinkage void bootblock_main(uint32_t bist, uint32_t tsc_lo, uint32_t tsc_hi);
 
 asmlinkage void bootblock_main(uint32_t bist, uint32_t tsc_lo, uint32_t tsc_hi)
 {
-	if (IS_ENABLED(CONFIG_BOOTBLOCK_CONSOLE))
-		console_init();
-
 	/*
 	 * TODO: We might want a cleaner way to handle BIST reporting before we
 	 * can converge with the generic bootblock in lib/.
 	 */
 	report_bist_failure(bist);
 
-	run_romstage();
-
-	/* Should never be reached, but halt just in case. */
-	halt();
+	/* Now jump to the generic bootblock and be on your merry way. */
+	main();
 }
